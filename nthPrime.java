7e5aa5cfc0262e40d6c7d5eab6a997ddf23a94db
Ridhima0827 <ridhima.gla_cs25@gla.ac.in>
2026-02-19 16:05:22 +0530

    Fix formatting issues in nthPrime.java

--- nthPrime.java
+++ nthPrime.java
@@ -5,7 +5,7 @@ public int NthPrime(int input1){
 int count=0;
 int num=1;
 while(count<input1){
-num++:
+num++;
 boolean prime=true;
 for(int i=2;i<num;i++){
 if(num%i==0){
@@ -18,4 +18,5 @@ count++;
 }
 return num;
 }
+
 }
